dcb0ef220a3f4084d08ac09e42b1a17ddc5ea87b
ferluchin <lgrandab@gmail.com>
2020-06-17 16:32:15 -0500

    Setting app flutter 2020
    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+// I need to import a helper library from flutter to get content on the screen
+import 'package:flutter/material.dart';
+import 'src/app.dart';
+
+//Define a 'main' function to run when our app starts
+void main() {
+  //create a new text widget to show some text on the screen
+
+  runApp(App());
+// Take that widget and get it on the screen
+}
---
+++ lib/src/app.dart
@@ -0,0 +1,48 @@
+// Import flutter helper library
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' show get;
+import 'models/image_model.dart';
+import 'dart:convert';
+
+//Create a class that will be our custom widget
+
+//this class must extend the 'StatlessWidget' base class
+
+class App extends StatefulWidget {
+  createState() {
+    return AppState();
+  }
+}
+
+class AppState extends State<App> {
+// Must define a build method that return the widget that *this* widget will show
+  int counter = 0;
+  List<ImageModel> images = [];
+
+  void fetchImage() async {
+    counter++;
+    var response =
+        await get('https://jsonplaceholder.typicode.com/photos/$counter');
+    var imageModel = ImageModel.fromJson(json.decode(response.body));
+
+    setState(() {
+      images.add(imageModel);
+    });
+  }
+
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Text('$counter'),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.airplanemode_active),
+          onPressed: fetchImage,
+        ),
+        appBar: AppBar(
+          title: Text('Lets see some images !'),
+        ),
+      ),
+    );
+  }
+}
